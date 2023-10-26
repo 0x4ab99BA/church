6318fe6c05f3cd8ab276101aacdb5e05606c072b
liubo <spongeliu0202@gmail.com>
2023-10-26 13:40:16 +1100

    added 'view more' button

--- website/views.py
+++ website/views.py
@@ -17,22 +17,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # @views.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
-    subscribed_groups = current_user.subscriptions
 
     if current_user.is_authenticated:
-        # 查询当前用户订阅的群组
-        user = User.query.get(current_user.id)
-        subscribed_groups = user.subscriptions
+        subscribed_groups = current_user.subscriptions
 
-        # 查询每个群组的最新帖子
         latest_posts = []
         for group in subscribed_groups:
-            latest_post = Post.query.filter_by(group_id=group.id).order_by(Post.created_at.desc()).first()
-            if latest_post:
-                latest_posts.append(latest_post)
+            group_posts = Post.query.filter_by(group_id=group.id).order_by(Post.created_at.desc()).limit(4).all()
+            if group_posts:
+                latest_posts.extend(group_posts)
+
+        latest_posts.sort(key=lambda post: post.created_at, reverse=True)
+        latest_4_posts = latest_posts[:4]
 
         return render_template('home.html',
-                               latest_posts=latest_posts,
+                               latest_posts=latest_4_posts,
                                user=current_user,
                                subscribed_groups=subscribed_groups)
 
